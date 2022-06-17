a4be62f22ee1e0d49de68c98d50fe5ef92eace09
Iza Ferreira <fsizabella25@gmail.com>
2022-06-17 01:43:16 -0300

    initial commit

---
+++ components/Header/index.tsx
@@ -0,0 +1,9 @@
+import * as Styled from './styles';
+
+export const Header = () => (
+  <Styled.Header>
+    <p>
+      Clone de interface do MercadoLivre
+    </p>
+  </Styled.Header>
+);
---
+++ components/Header/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  background-color: var(--color-header);
+  padding: 12px;
+  > p {
+    color: var(--color-gray);
+    text-align: center;
+    display: block;
+    margin: 0 auto;
+  }
+`
